4ec85f0cf942b1c0a60e792caeb2a13076642690
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-05 00:01:01 +0900

    メモの書き込みと読み込みに関するプロパティ

---
+++ Memo/Memo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by home on 2018/08/04.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var writeMemo: UITextField!
+    @IBOutlet weak var readMemo: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
