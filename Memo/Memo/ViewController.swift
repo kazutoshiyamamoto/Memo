f83534eb0944f4d2296739cdadc668b822541304
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-08 00:36:38 +0900

    テキストの保存に関するメソッドの修正

--- Memo/Memo/ViewController.swift
+++ Memo/Memo/ViewController.swift
@@ -11,12 +11,8 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var writeMemo: UITextField!
-    @IBOutlet weak var readMemoButton: UIButton!
     @IBOutlet weak var editCompletionButton: UIButton!
-    
-    
     @IBOutlet weak var readLabel: UILabel!
-    
     
     // 編集終了でキーボードを下げる
     @IBAction func tapView(_ sender: UITapGestureRecognizer) {
@@ -37,21 +33,22 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     // 編集中に変更があるたびに呼び出されるデリゲートメソッド
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let tmpStr = writeMemo.text! as NSString
-        let replaceString = tmpStr.replacingCharacters(in: range, with: string)
-        if replaceString != "" {
-            // ユーザーデフォルトに保存する
-            let defaults = UserDefaults.standard
-            defaults.set(tmpStr, forKey: "edit")
-            
-        } else {
-            // エラー処理or何もしない
-        }
-        // テキストフィールドを更新する
-        return true
-    }
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//        let tmpStr = writeMemo.text! as NSString
+//        let replaceString = tmpStr.replacingCharacters(in: range, with: string)
+//        if replaceString != "" {
+//            // ユーザーデフォルトに保存する
+//            let defaults = UserDefaults.standard
+//            defaults.set(tmpStr, forKey: "edit")
+//
+//        } else {
+//            // エラー処理
+//            print("Error")
+//        }
+//        // テキストフィールドを更新する
+//        return true
+//    }
     
     // クリアボタンがタップされると呼ばれるデリゲートメソッド
     func textFieldShouldClear(_ textField: UITextField) -> Bool {
@@ -59,11 +56,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    // 保存したテキストをラベルに表示する
     @IBAction func readRecord(_ sender: Any) {
-        
-        
+        let defaults = UserDefaults.standard
+        let value = defaults.string(forKey: "edit")
+        readLabel.text = value
     }
     
+    @IBAction func testButton(_ sender: Any) {
+        let tmpStr = writeMemo.text! as NSString
+            // ユーザーデフォルトに保存する
+            let defaults = UserDefaults.standard
+            defaults.set(tmpStr, forKey: "edit")
+    }
     @IBAction func editCompletionButton(_ sender: Any) {
         view.endEditing(true)
         
