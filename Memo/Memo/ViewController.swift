723060f0b39048a2c219279181e942fc976a1fa2
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-08 23:49:46 +0900

    キーボードを下げるボタンの削除

--- Memo/Memo/ViewController.swift
+++ Memo/Memo/ViewController.swift
@@ -11,49 +11,25 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var writeMemo: UITextField!
-    @IBOutlet weak var editCompletionButton: UIButton!
     @IBOutlet weak var readLabel: UILabel!
-    
-    // 編集終了でキーボードを下げる
-    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
-        view.endEditing(true)
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         writeMemo.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    // 編集中に変更があるたびに呼び出されるデリゲートメソッド
-//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-//
-//        let tmpStr = writeMemo.text! as NSString
-//        let replaceString = tmpStr.replacingCharacters(in: range, with: string)
-//        if replaceString != "" {
-//            // ユーザーデフォルトに保存する
-//            let defaults = UserDefaults.standard
-//            defaults.set(tmpStr, forKey: "edit")
-//
-//        } else {
-//            // エラー処理
-//            print("Error")
-//        }
-//        // テキストフィールドを更新する
-//        return true
-//    }
-    
-    // クリアボタンがタップされると呼ばれるデリゲートメソッド
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        writeMemo.text = ""
-        return true
+    // ユーザーデフォルトに保存する
+    @IBAction func testButton(_ sender: Any) {
+        let tmpStr = writeMemo.text! as NSString
+        let defaults = UserDefaults.standard
+        defaults.set(tmpStr, forKey: "edit")
+        view.endEditing(true)
     }
     
     // 保存したテキストをラベルに表示する
@@ -63,16 +39,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
         readLabel.text = value
     }
     
-    @IBAction func testButton(_ sender: Any) {
-        let tmpStr = writeMemo.text! as NSString
-            // ユーザーデフォルトに保存する
-            let defaults = UserDefaults.standard
-            defaults.set(tmpStr, forKey: "edit")
-    }
-    @IBAction func editCompletionButton(_ sender: Any) {
+    // 編集終了でキーボードを下げる
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
-        
     }
+    
+    // 編集中に変更があるたびに呼び出されるデリゲートメソッド
+    //    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    //
+    //        let tmpStr = writeMemo.text! as NSString
+    //        let replaceString = tmpStr.replacingCharacters(in: range, with: string)
+    //        if replaceString != "" {
+    //            // ユーザーデフォルトに保存する
+    //            let defaults = UserDefaults.standard
+    //            defaults.set(tmpStr, forKey: "edit")
+    //        } else {
+    //            // エラー処理
+    //            print("Error")
+    //        }
+    //        // テキストフィールドを更新する
+    //        return true
+    //    }
+    
+    // クリアボタンがタップされると呼ばれるデリゲートメソッド
+    //    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+    //        writeMemo.text = ""
+    //        return true
+    //    }
     
 }
 
