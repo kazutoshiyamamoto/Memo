dd14667989ca70ca608e7e9660878617bf3b0ccf
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-08-12 00:51:22 +0900

    入力予測や変換もユーザーデフォルトに保存できるよう変更

--- Memo/Memo/ViewController.swift
+++ Memo/Memo/ViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
-
+class ViewController: UIViewController {
+    
     @IBOutlet weak var textMemoryField: UITextField!
     @IBOutlet weak var showLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        textMemoryField.delegate = self
+        
+//        textMemoryField.delegate = self
+        
+        // textFieldDidChangeを識別するよう指定する
+        textMemoryField.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)), for: UIControlEvents.editingChanged)
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,13 +29,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    // ユーザーデフォルトに保存する
-    @IBAction func memorizeText(_ sender: Any) {
+    // 入力したテキストをユーザーデフォルトに保存
+    @objc func textFieldDidChange(_ textField: UITextField) {
         let writtenText = textMemoryField.text! as NSString
         let defaults = UserDefaults.standard
         defaults.set(writtenText, forKey: "memo")
-        view.endEditing(true)
     }
+    
+    // 編集中に変更があるたびに呼び出されるデリゲートメソッド（変換が適用されない）
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//
+//        let writtenText = textMemoryField.text! as NSString
+//        let replaceString = writtenText.replacingCharacters(in: range, with: string)
+//        if replaceString != "" {
+//            // ユーザーデフォルトに保存する
+//            let defaults = UserDefaults.standard
+//            defaults.set(writtenText, forKey: "memo")
+//        } else {
+//            print("Error")
+//        }
+//        return true
+//    }
+    
+//     ユーザーデフォルトに保存する
+//        @IBAction func memorizeText(_ sender: Any) {
+//            let writtenText = textMemoryField.text! as NSString
+//            let defaults = UserDefaults.standard
+//            defaults.set(writtenText, forKey: "memo")
+//            view.endEditing(true)
+//        }
+    
     // 保存したテキストをラベルに表示する
     @IBAction func readText(_ sender: Any) {
         let defaults = UserDefaults.standard
@@ -41,23 +69,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func tapView(_ sender: UITapGestureRecognizer) {
         view.endEditing(true)
     }
-    
-     // 編集中に変更があるたびに呼び出されるデリゲートメソッド（変換が適用されない）
-        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-    
-            let writtenText = textMemoryField.text! as NSString
-            let replaceString = writtenText.replacingCharacters(in: range, with: string)
-            if replaceString != "" {
-                // ユーザーデフォルトに保存する
-                let defaults = UserDefaults.standard
-                defaults.set(writtenText, forKey: "memo")
-            } else {
-                // エラー処理
-                print("Error")
-            }
-            // テキストフィールドを更新する
-            return true
-        }
     
     // クリアボタンがタップされると呼ばれるデリゲートメソッド
     //    func textFieldShouldClear(_ textField: UITextField) -> Bool {
